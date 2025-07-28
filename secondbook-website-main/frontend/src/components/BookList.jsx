b60c39f024a4109e0e35a65269d7d69e91ef19f0
hengputh52 <harrystyles10203040@gmail.com>
2025-07-28 20:48:55 +0700

    update

--- secondbook-website-main/frontend/src/components/BookList.jsx
+++ secondbook-website-main/frontend/src/components/BookList.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../styles/BookList.css";
-import { fetchFilteredBooks } from "../api/bookApi";
+import { fetchFilteredBooks } from "../service/bookApi";
 
 const BookList = ({ filter, searchTerm }) => {
   const [books, setBooks] = useState([]);
--- secondbook-website-main/frontend/src/components/BookSciFi.jsx
+++ secondbook-website-main/frontend/src/components/BookSciFi.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom"; // Import Link for navigation
-import { books } from "./BookList";
+
 import "../styles/BookList.css";
 
 const BookSciFi = () => {
